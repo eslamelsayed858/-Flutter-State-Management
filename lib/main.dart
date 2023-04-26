3f6036dd72bf20ba95b8c7d428ead52bc08ac4bc
eslamelsayed858 <ee9122758@gmail.com>
2023-04-26 05:42:53 +0200

    updatea

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ShoppingCart(
-      child: MaterialApp(
+      child: const MaterialApp(
         debugShowCheckedModeBanner: false,
         home: ScrenApp(),
       ),
